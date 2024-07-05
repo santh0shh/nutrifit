10ab4a2b2ad57b15bf4ca55983e685e308c25501
santh0shh <mr.santhosh116@gmail.com>
2024-07-05 20:09:38 +0530

    Update scroll.js

--- scroll.js
+++ scroll.js
@@ -39,7 +39,7 @@ window.onscroll = function(){
         }
         h += document.getElementById('exer').scrollHeight;
         if(document.innerWidth > 1000 && document.innerWidth < document.innerHeight){
-            h += 400;
+            h -= 300;
             alert("hi");
         }
     }
